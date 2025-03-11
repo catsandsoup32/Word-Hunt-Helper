fc72876d9628843c86534139c7f41e05a935cd62
Edmund Bu <2edmundbu@gmail.com>
2025-03-11 00:33:17 -0700

    Made solver function modular for future use

--- solver.py
+++ solver.py
@@ -4,16 +4,6 @@ import random, copy
 
 
 NUM_ROWS = NUM_COLS = 4
-grid = []
-
-print("(all lowercase input)")
-for i in range(1, NUM_ROWS+1):
-    n = 0
-    while (n != NUM_ROWS):
-        row = input(f"Enter row {i}: ") 
-        n = len(row)
-        grid.append(list(row))
-
 
 root = TrieNode()
 with open("word_list.txt", "r") as f:
@@ -21,9 +11,8 @@ with open("word_list.txt", "r") as f:
         line = line.strip() 
         trie.insert(root, line)
 
-
 directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
-def get_valid_directions(curr_pos, positions, node):
+def get_valid_directions(grid, curr_pos, positions, node):
     """Returns a list of (x,y) coords that are in-bounds, non-visited, and that are viable prefixes"""
     valid_directions = []
 
@@ -35,9 +24,11 @@ def get_valid_directions(curr_pos, positions, node):
     return valid_directions
 
 
-def search_all(curr_pos, positions, prefix, node):
+def search_all(grid, word_hash, curr_pos, positions, prefix, node):
     """
     Args:
+        grid (2-D array): Board represented as grid
+        word_hash (dict): Maps each word to a set of positions
         curr_pos (tuple): Row and column
         positions (set of tuples): Positions up to current, inclusive  
         prefix (string): Prefix including current char
@@ -46,40 +37,66 @@ def search_all(curr_pos, positions, prefix, node):
     if node.end_of_word and not (prefix in word_hash): 
         word_hash[prefix] = tuple(positions)
 
-    valid_directions = get_valid_directions(curr_pos, positions, node) 
+    valid_directions = get_valid_directions(grid, curr_pos, positions, node) 
     if not valid_directions: 
         return 
     
     for row, col in valid_directions:
         positions[(row, col)] = None
         new_char = grid[row][col]
-        search_all((row, col), positions, prefix + new_char, node.children[ord(new_char) - ord('a')])
+        search_all(grid, word_hash, (row, col), positions, prefix + new_char, node.children[ord(new_char) - ord('a')])
         positions.pop((row, col))
-        
-    
-word_hash = {}
-for row in range(NUM_ROWS):
-    for col in range(NUM_COLS):
-        positions = {}
-        positions[(row, col)] = None
-        curr_char = grid[row][col]
-        search_all((row, col), positions, curr_char, root.children[ord(curr_char)-ord('a')])
 
 
-sorted_hash = sorted(word_hash.items(), key=lambda item: len(item[0]), reverse=True)
-print(sorted_hash)
+def solve(terminal: bool, pause_print: bool):
+    """
+    Args: 
+        terminal: Whether to enter characters in terminal or to infer from photo
+        pause_print: Whether to print solutions drawn on grid with pauses between each
+    """
+    grid = []
+    word_hash = {}
 
-for word, positions in sorted_hash:
-    temp_grid = copy.deepcopy(grid)
-    for i, coord in enumerate(positions):
-        r, c = coord[0], coord[1]
-        curr_char = temp_grid[r][c]
-        temp_grid[r][c] = "\033[31m" + curr_char + "\033[0m"
+    if terminal: 
+        print("(all lowercase input)")
+        for i in range(1, NUM_ROWS+1):
+            n = 0
+            while (n != NUM_ROWS):
+                row = input(f"Enter row {i}: ") 
+                n = len(row)
+                grid.append(list(row))
+    else:
+        # Put camera code here
+        pass
     
-    print("\033[31m" + word + "\033[0m")
-    for i in range(NUM_ROWS):
-        print("   ".join(temp_grid[i]))
-    
-    input("Press enter for next")
+    for row in range(NUM_ROWS):
+        for col in range(NUM_COLS):
+            positions = {}
+            positions[(row, col)] = None
+            curr_char = grid[row][col]
+            search_all(grid, word_hash, (row, col), positions, curr_char, root.children[ord(curr_char)-ord('a')])
+
+
+    sorted_hash = sorted(word_hash.items(), key=lambda item: len(item[0]), reverse=True)
+    print(sorted_hash)
+
+    if pause_print:
+        for word, positions in sorted_hash:
+            temp_grid = copy.deepcopy(grid)
+            for i, coord in enumerate(positions):
+                r, c = coord[0], coord[1]
+                curr_char = temp_grid[r][c]
+                temp_grid[r][c] = "\033[31m" + curr_char + "\033[0m"
+            
+            print("\033[31m" + word + "\033[0m")
+            for i in range(NUM_ROWS):
+                print("   ".join(temp_grid[i]))
+            
+            input("Press enter for next")
 
     
+if __name__ == "__main__":
+    solve(
+        terminal = True,
+        pause_print = False
+    )
